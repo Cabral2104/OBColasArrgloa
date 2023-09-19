a55c582a479fb9fad844a19cf90d10d2a74fb53d
Cabral2104 <valdezemilio50@gmail.com>
2023-09-18 19:29:36 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBColasArrgloa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cola cola = new Cola(5);
+
+            cola.Encolar(1);
+            cola.Encolar(2);
+            cola.Encolar(3);
+
+            Console.WriteLine(cola.Tamaño()); // 3
+            Console.WriteLine(cola.Desencolar()); // 1
+            Console.WriteLine(cola.Frente()); // 2
+
+            cola.Encolar(4);
+            Console.WriteLine(cola.Tamaño()); // 3
+            Console.ReadKey();
+        }
+    }
+}
---
+++ cola.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBColasArrgloa
+{
+    internal class Cola
+    {
+        int[] arreglo;
+        int inicio;
+        int final;
+        int contador;
+
+        public Cola(int tam)
+        {
+            arreglo = new int[tam];
+            inicio = 0;
+            final = -1;
+            contador = 0;
+        }
+
+        public void Encolar(int x)
+        {
+            if (final == arreglo.Length - 1)
+            {
+                final = -1;
+            }
+            arreglo[++final] = x;
+            contador++;
+        }
+
+        public int Desencolar()
+        {
+            if (inicio > final)
+            {
+                throw new InvalidOperationException("Cola vacía");
+            }
+            int x = arreglo[inicio++];
+            contador--;
+            return x;
+        }
+
+        public int Frente()
+        {
+            if (inicio > final)
+            {
+                throw new InvalidOperationException("Cola vacía");
+            }
+            return arreglo[inicio];
+        }
+
+        public int Tamaño()
+        {
+            return contador;
+        }
+    }
+}
